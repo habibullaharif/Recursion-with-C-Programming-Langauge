269959572cbdc39d54d1e9470525e215376bcb5f
habibullaharif <77946331+habibullaharif@users.noreply.github.com>
2021-07-20 20:35:55 +0100

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -5,9 +5,10 @@ int factorial(int);
 int main(void)
 {
   int num; printf("Enter a number: "); scanf("%d",&num);
+  
   int ret = factorial(num);
 
-  printf("%d",ret);
+  printf("The factorial Of %d: %d",num,ret);
 
   return 0;
 }
