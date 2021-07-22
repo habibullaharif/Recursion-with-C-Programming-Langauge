eec17020d2d26a27e74cc1fbc0b8f52d92d9e05f
habibullaharif <77946331+habibullaharif@users.noreply.github.com>
2021-07-22 02:45:01 +0100

    Update addEvenNaturalNumber.c

--- addEvenNaturalNumber.c
+++ addEvenNaturalNumber.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int addEvenNaturalNumber(int);
+
+int main(void)
+{
+  int num; printf("Enter a limit: "); scanf("%d",&num);
+
+  int ret = addEvenNaturalNumber(num);
+
+  printf("%d",ret);
+
+  return 0;
+}
+
+int addEvenNaturalNumber(int n) {
+  if (n == 0 || n == 1) {
+    return 0;
+  }
+  if (n % 2 == 1) {
+    n--;
+  }
+  return n + addEvenNaturalNumber(n-2);
+}
 
